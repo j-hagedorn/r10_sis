eed9e3f95edf58ca2e6543b5221c6355406c797d
Joshh <j-hagedorn@users.noreply.github.com>
2019-05-10 14:08:04 -0400

    Revise based on feedback

--- prep/getSISelig.R
+++ prep/getSISelig.R
@@ -55,10 +55,7 @@ combined <-
     sis_complete_elig = is.na(sis_completed_dt) == F & sis_eligible == T,
     # SIS assessments completed for ineligible individuals
     sis_complete_inel = is.na(sis_completed_dt) == F & sis_eligible == F,
-    sis_overdue  = (sis_completed_dt + (365 * 3)) < today(),
-    sis_coming90 = 
-      (sis_completed_dt + (365 * 3)) < (today() + 90)
-      & (sis_completed_dt + (365 * 3)) >= today() 
+    sis_complete_any = sis_complete_elig == T | sis_complete_inel == T 
   ) %>%
   # Include individuals who are eligible OR who have received a SIS
   filter(sis_eligible == T | sis_complete_inel == T)  %>%
@@ -70,32 +67,51 @@ combined <-
     end_of_fy = ymd(
       paste0(
         # Identify FY of current date
-        as.POSIXlt(today())$year + (as.POSIXlt(today())$mo >= 4) + 1900,
+        as.POSIXlt(today())$year + (as.POSIXlt(today())$mo >= 10) + 1900,
         # then add MM/DD
         "-09-30"
       )
     ),
-    due_by_dt = agency_admission_date + years(3),
+    due_by_dt = case_when(
+      is.na(sis_completed_dt) & deferral_active == F ~ as.Date(defer_date + years(1)),
+      is.na(sis_completed_dt)      ~ as.Date(agency_admission_date + months(9)),
+      is.na(agency_admission_date) ~ as.Date(sis_completed_dt + years(3)),
+      sis_completed_dt >= agency_admission_date ~ as.Date(sis_completed_dt + years(3)),
+      sis_completed_dt < agency_admission_date ~ as.Date(sis_completed_dt + years(3))
+    ),
+    due_by_dt = if_else(due_by_dt < ymd("2017-09-30"),ymd("2017-09-30"),due_by_dt),
+    sis_overdue  = due_by_dt < today() & !is.na(sis_completed_dt),
+    sis_coming90 = due_by_dt < (today() + 90) & due_by_dt >= today(),
     due_in_fy = due_by_dt <= end_of_fy
   ) %>%
-  select(-end_of_fy,-due_by_dt)
+  # select(-end_of_fy,-due_by_dt) %>%
+  select(
+    MEDICAID_ID,PROVIDER_NAME,
+    ends_with("_dt"),ends_with("_date"),ends_with("_service"),
+    starts_with("eligible"),
+    everything()
+  )
   
 # Filter to only include those who need SIS
 need_sis <-
   combined %>%
   filter(is.na(PROVIDER_NAME) == F) %>%
   select(
-    MEDICAID_ID,PROVIDER_NAME,agency_admission_date,most_recent_service,
-    sis_completed_dt:sis_coming90,due_in_fy,deferral_active
+    MEDICAID_ID,PROVIDER_NAME,agency_admission_date,most_recent_service,due_by_dt,
+    sis_completed_dt,sis_complete_any,sis_complete_inel,sis_overdue,
+    sis_coming90,due_in_fy,deferral_active
   ) %>%
   mutate(
     # Tag individuals showing NA admit date. They are found on through the eligibility or 
     # completed SIS data, but are closed as of the date the BH-TEDS report was pulled
     # (therefore not listed on the BH-TEDS report and lacking agency admit field)
-    closed_in_pce = is.na(agency_admission_date),
-    sis_complete_any = sis_complete_elig == T | sis_complete_inel == T
+    closed_in_pce = is.na(agency_admission_date)
   ) %>%
-  gather(field,val,sis_eligible:sis_complete_any) %>%
+  select(
+    MEDICAID_ID,PROVIDER_NAME,agency_admission_date,most_recent_service,due_by_dt,
+    starts_with("sis_"),due_in_fy,deferral_active
+  ) %>%
+  gather(field,val,sis_complete_any:deferral_active) %>%
   mutate(
     status = case_when(
       field == "sis_complete_any"  & val == F  ~ "Initial SIS Needed",
@@ -106,15 +122,6 @@ need_sis <-
       field == "deferral_active"   & val == T  ~ "Active Deferral",
       field == "deferral_active"   & val == F  ~ "Expired Deferral",
       field == "closed_in_pce"     & val == T  ~ "Closed in PCE MiX system"
-    )
-  ) %>%
-  mutate(
-    # Flag SIS due date
-    sis_due_dt = case_when(
-      # Add initial go-live
-      is.na(sis_completed_dt) & agency_admission_date < ymd("2014-07-01") ~ ymd("2017-09-30"),
-      is.na(sis_completed_dt) == T ~ ymd(agency_admission_date) + years(3),
-      is.na(sis_completed_dt) == F ~ ymd(sis_completed_dt) + years(3)
     )
   ) %>%
   filter(is.na(status) == F) %>%
