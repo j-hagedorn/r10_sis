bf20cf13aed801e5ec8e2608bba36f8f97c9539c
Joshh <j-hagedorn@users.noreply.github.com>
2019-12-10 15:53:59 -0500

    Update getSISelig.R

--- prep/getSISelig.R
+++ prep/getSISelig.R
@@ -73,7 +73,8 @@ combined <-
       )
     ),
     due_by_dt = case_when(
-      deferral == T    ~ as.Date(defer_date + years(1)),
+      sis_completed_dt >= defer_date                 ~ as.Date(sis_completed_dt + years(3)),
+      deferral_active == T                           ~ as.Date(defer_date + years(1)),
       is.na(sis_completed_dt)                        ~ as.Date(agency_admission_date + months(9)),
       is.na(agency_admission_date)                   ~ as.Date(sis_completed_dt + years(3)),
       sis_completed_dt >= agency_admission_date      ~ as.Date(sis_completed_dt + years(3)),
